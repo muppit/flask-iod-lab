deb10d0f4756a1a9abc2cb73eda2204a32fbc160
muppit <139455285+muppit@users.noreply.github.com>
2023-11-21 18:36:09 +0800

    Version 1

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+import os
+
+import joblib
+from utils import preprocessor
+
+app = Flask(__name__)
+model = joblib.load('model.joblib')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    input_text = request.form['text']
+
+    # Assuming your model expects a DataFrame for the input
+    data = pd.DataFrame([input_text], columns=['text'])
+
+    # Preprocess and predict
+    predicted_sentiment = model.predict(data)[0]
+    if predicted_sentiment == 1:
+        output = 'positive'
+    else:
+        output = 'negative'
+
+    return render_template('result.html', sentiment=f'Predicted sentiment of "{input_text}" is {output}.')
+
+
+if __name__ == "__main__":
+    host = os.getenv('FLASK_HOST', '127.0.0.1')
+    port = os.getenv('FLASK_PORT', '5001')
+    app.run(host=host, port=int(port), debug=True)
---
+++ utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import regex as re
+import joblib
+import en_core_web_sm
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.svm import LinearSVC
+
+nlp = en_core_web_sm.load()
+classifier = LinearSVC()
+
+def clean_text(text):
+    text = str(text)
+    # reduce multiple spaces and newlines to only one
+    text = re.sub(r'(\s\s+|\n\n+)', r'\1', text)
+    # remove double quotes
+    text = re.sub(r'"', '', text)
+    return text
+	
+def convert_text(text):
+    sent = nlp(text)
+    ents = {x.text: x for x in sent.ents}
+    tokens = []
+    for w in sent:
+        if w.is_stop or w.is_punct:
+            continue
+        if w.text in ents:
+            tokens.append(w.text)
+        else:
+            tokens.append(w.lemma_.lower())
+    text = ' '.join(tokens)
+
+    return text
+
+
+class preprocessor(TransformerMixin, BaseEstimator):
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        if isinstance(X, pd.DataFrame) or isinstance(X, pd.Series):
+            # If X is a DataFrame or Series, use apply
+            return X.apply(clean_text).apply(convert_text)
+        elif isinstance(X, str):
+            # If X is a string, directly apply the functions
+            return convert_text(clean_text(X))
+        else:
+            # Handle other types (like lists or numpy arrays) as needed
+            # You may need to convert them into a format that can be processed
+            raise TypeError("Input type not supported")
